feabea883ef30dad7c9765ad0235781207ccb5a3
Thomas Mathers <thomas.mathers@ideanotion.net>
2022-09-09 21:32:03 -0400

    changes

--- src/lib/fetch/use-fetch.ts
+++ src/lib/fetch/use-fetch.ts
@@ -1,32 +1,45 @@
 import { useCallback, useState } from "react";
 
-interface Props {
-    url: string;
-    options?: RequestInit;
-    executeAutomatically?: boolean;
+type FetchFunction<T> = () => Promise<T>;
+
+interface HttpRequest {
+  url: string;
+  options?: RequestInit;
 }
 
-function useFetch<T>(props: Props) {
-    const { url, options, executeAutomatically } = props;
-    const [fetching, setFetching] = useState<boolean>();
-    const [response, setResponse] = useState<T>();
-    const execute = useCallback(async () => {
-        try {
-            setFetching(true);
-            setResponse(undefined);
-            const response = await fetch(url, options);
-            const json = await response.json();
-            const responseBody: T = json;
-            setResponse(responseBody);
-            return responseBody;
-        } finally {
-            setFetching(false);
-        }
-    }, [url, options]);        
-    if (executeAutomatically) {
-        execute();
+function createFetchFunctionFromHttpRequest<T>(httpRequest: HttpRequest): FetchFunction<T> {
+  const { url, options } = httpRequest;
+  return async () => {
+    const response = await fetch(url, options);
+    return await response.json();
+  }
+}
+
+interface Props<T> {
+  func: FetchFunction<T>
+  executeAutomatically?: boolean;
+}
+
+function useFetch<T>(props: Props<T>) {
+  const { func, executeAutomatically } = props;
+  const [fetching, setFetching] = useState<boolean>();
+  const [response, setResponse] = useState<T>();
+  const execute = useCallback(async () => {
+    try {
+      setFetching(true);
+      setResponse(undefined);
+      const responseBody: T = await func();
+      setResponse(responseBody);
+      return responseBody;
+    } finally {
+      setFetching(false);
     }
-    return { fetching, response, execute };
+  }, [func]);
+  if (executeAutomatically) {
+    execute();
+  }
+  return { fetching, response, execute };
 }
 
 export default useFetch;
+export { createFetchFunctionFromHttpRequest }
